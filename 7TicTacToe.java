e2c20eb1f2203b612600659a01073bd37fecbeb4
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2025-08-14 08:02:28 +0530

    addded the tictactoe games

---
+++ 7TicTacToe.java
@@ -0,0 +1,245 @@
+// imp for less experience people
+/*
+All objects
+1. piece symbol
+2. n X n boat
+3. players
+ */
+/*
+Work of each object : -> 
+
+enum piecetype { 0,X}
+plaingPiece just set the piece type
+Player -> holds name and piece type set and get piece and name (although we set in constructor)
+board -> add list, get free space, print board
+tictactoe game -> main player creation and turn and also start game functionality, is There winner check
+main -> new titactoe game and intitialixe with start game 
+*/
+public enum PieceType{
+    X, O;
+}
+
+public class PlayingPiece{
+    public PieceType pieceType;
+    PlayingPiece(PieceType obj){
+        this.pieceType=obj;
+    }
+}
+
+public class PlayingPieceX extends PlayingPiece {
+   public PlayingPieceX(){
+    super(PieceType.X)
+   }
+}
+
+public class PlayingPieceO extends PlayingPiece {
+   public PlayingPieceO(){
+    super(PieceType.O)
+   }
+}
+
+//Now see we have player and for each player turn we use dequeue (has a with playing piece)
+public class Player {
+
+    public String name;
+    public PlayingPiece playingPiece;
+
+    public Player(String name, PlayingPiece playingPiece) {
+        this.name = name;
+        this.playingPiece = playingPiece;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PlayingPiece getPlayingPiece() {
+        return playingPiece;
+    }
+
+    public void setPlayingPiece(PlayingPiece playingPiece) {
+        this.playingPiece = playingPiece;
+    }
+}
+
+
+//now board also have has a playing piece
+public class Board {
+
+    public int size;
+    public PlayingPiece[][]board;
+
+    public Board(int size) {
+        this.size = size;
+        board = new PlayingPiece[size][size];
+    }
+
+
+    public boolean addPiece(int row, int column, PlayingPiece playingPiece) {
+
+        if(board[row][column] != null) {
+            return false;
+        }
+        board[row][column] = playingPiece;
+        return true;
+    }
+
+
+    public List<Pair<Integer, Integer>> getFreeCells() {
+        List<Pair<Integer, Integer>> freeCells = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] == null) {
+                    Pair<Integer, Integer> rowColumn = new Pair<>(i, j);
+                    freeCells.add(rowColumn);
+                }
+            }
+        }
+
+        return freeCells;
+    }
+
+    public void printBoard() {
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] != null) {
+                   System.out.print(board[i][j].pieceType.name() + "   ");
+                } else {
+                    System.out.print("    ");
+
+                }
+                System.out.print(" | ");
+            }
+            System.out.println();
+
+        }
+    }
+}
+
+//Main Tic Tac Toe game (has a with both board and Players)
+public class TicTacToeGame {
+
+    Deque<Player> players;
+    Board gameBoard;
+
+
+    public void initializeGame(){
+
+        //creating 2 Players
+       players = new LinkedList<>();
+        PlayingPieceX crossPiece = new PlayingPieceX();
+        Player player1 = new Player("Player1", crossPiece);
+
+        PlayingPieceO noughtsPiece = new PlayingPieceO();
+        Player player2 = new Player("Player2", noughtsPiece);
+
+        players.add(player1);
+        players.add(player2);
+
+        //initializeBoard
+        gameBoard = new Board(3);
+    }
+
+    public String startGame(){
+
+        boolean noWinner = true;
+        while(noWinner){
+
+            //take out the player whose turn is and also put the player in the list back
+            Player playerTurn = players.removeFirst();
+
+            //get the free space from the board
+            gameBoard.printBoard();
+            List<Pair<Integer, Integer>> freeSpaces =  gameBoard.getFreeCells();
+            if(freeSpaces.isEmpty()) {
+                noWinner = false;
+                continue;
+            }
+
+            //read the user input
+            System.out.print("Player:" + playerTurn.name + " Enter row,column: ");
+            Scanner inputScanner = new Scanner(System.in);
+            String s = inputScanner.nextLine();
+            String[] values = s.split(",");
+            int inputRow = Integer.valueOf(values[0]);
+            int inputColumn = Integer.valueOf(values[1]);
+
+
+            //place the piece
+            boolean pieceAddedSuccessfully = gameBoard.addPiece(inputRow,inputColumn, playerTurn.playingPiece);
+            if(!pieceAddedSuccessfully) {
+                //player can not insert the piece into this cell, player has to choose another cell
+                System.out.println("Incorredt possition chosen, try again");
+                players.addFirst(playerTurn);
+                continue;
+            }
+            players.addLast(playerTurn);
+
+            boolean winner = isThereWinner(inputRow, inputColumn, playerTurn.playingPiece.pieceType);
+            if(winner) {
+                return playerTurn.name;
+            }
+        }
+
+        return "tie";
+    }
+
+    public boolean isThereWinner(int row, int column, PieceType pieceType) {
+
+        boolean rowMatch = true;
+        boolean columnMatch = true;
+        boolean diagonalMatch = true;
+        boolean antiDiagonalMatch = true;
+
+        //need to check in row
+        for(int i=0;i<gameBoard.size;i++) {
+
+            if(gameBoard.board[row][i] == null || gameBoard.board[row][i].pieceType != pieceType) {
+                rowMatch = false;
+            }
+        }
+
+        //need to check in column
+        for(int i=0;i<gameBoard.size;i++) {
+
+            if(gameBoard.board[i][column] == null || gameBoard.board[i][column].pieceType != pieceType) {
+                columnMatch = false;
+            }
+        }
+
+        //need to check diagonals
+        for(int i=0, j=0; i<gameBoard.size;i++,j++) {
+            if (gameBoard.board[i][j] == null || gameBoard.board[i][j].pieceType != pieceType) {
+                diagonalMatch = false;
+            }
+        }
+
+        //need to check anti-diagonals
+        for(int i=0, j=gameBoard.size-1; i<gameBoard.size;i++,j--) {
+            if (gameBoard.board[i][j] == null || gameBoard.board[i][j].pieceType != pieceType) {
+                antiDiagonalMatch = false;
+            }
+        }
+
+        return rowMatch || columnMatch || diagonalMatch || antiDiagonalMatch;
+    }
+
+}
+
+
+
+//Main file 
+public class Main {
+
+    public static void main(String args[]) {
+        TicTacToeGame game = new TicTacToeGame();
+        game.initializeGame();
+        System.out.println("game winner is: " + game.startGame());
+    }
+}
